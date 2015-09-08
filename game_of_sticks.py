798b79168089b1aab5b9339eb9cedb9b9702ecd0
Bob Amand <biotmg@nc.rr.com>
2015-09-08 18:46:27 -0400

    draft 08-Sep-2015 6:45pm

--- game_of_sticks.py
+++ game_of_sticks.py
@@ -2,26 +2,6 @@
 import re
 import random
 
-# def stick_status(sticks, Player1, Player2):
-#
-# """
-#     keeps track of how many sticks are in play.
-#     inputs are player identifiers (AI, poeple) and pile start
-#     - starting amount
-#     - play1 pick
-#     - play2 pick
-#     -
-#
-# """
-#     # TODO
-#     pass
-#
-# def game_display(arg):
-#
-#     # TODO
-#     pass
-#
-
 def confirm_input(take, min, max):
     if min <= take <= max:
         return True
@@ -34,17 +14,22 @@ def starting_stick_parameters(start_stick, low, high):
     print("Please select a number of sticks between {} and {}. ". format(low, high))
     return False
 
-
 def main():
-    print("-- Welcome to the Game of Sticks --\n")
+    print("\n-- Welcome to the Game of Sticks --\n")
 # Current parameters are:
     min = 1
     max = 3
     start_stick = 10
-    # print("Select the number of sticks between How many sticks? ")
+    low = 10
+    high = 100
+    print("How many sticks to start between {} and {} ? \n". format(low, high))
+    start_stick = int(input("How many sticks do you want? "))
+    if low > start_stick > high:
+        starting_stick_parameters(start_stick, low, high)
+
     play1 = 'Player 1'
     play2 = 'Player 2'
-    play_list = ('Player 1', 'Player 2')  
+    play_list = ('Player 1', 'Player 2')
 # Core game_play:
     while start_stick > 0:
         for p in range(len(play_list)):
@@ -52,9 +37,9 @@ def main():
             print("     Take away between {} and {} sticks\n". format(min, max))
             print("{}:". format(play_list[p]))
             take = int(input("  Selection:  "))
-            while start_stick - take < 0:
+                #confirm_input(take, min, max)
+            while confirm_input(take, min, max) == False or start_stick - take < 0:
                 take = int(input("  Try another Selection:  "))
-            # possible insertion of confirm_input
             start_stick = start_stick - take
 
     print("{} loses! - Play again". format(play_list[p]))
